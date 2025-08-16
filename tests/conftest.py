52e52718d83aaaae2015ddd48e8b596d3eb472cb
Nick Crews <nickcrews35@gmail.com>
2025-08-16 19:26:22 -0400

    More slimming

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,12 +5,6 @@ import requests_mock
 from requests_mock import Mocker
 
 from alpaca.broker.client import BrokerClient
-from alpaca.data.historical import StockHistoricalDataClient
-from alpaca.data.historical.corporate_actions import CorporateActionsClient
-from alpaca.data.historical.crypto import CryptoHistoricalDataClient
-from alpaca.data.historical.news import NewsClient
-from alpaca.data.historical.option import OptionHistoricalDataClient
-from alpaca.data.historical.screener import ScreenerClient
 from alpaca.trading.client import TradingClient
 
 pytest_plugins = ("pytest_asyncio",)
@@ -43,54 +37,3 @@ def trading_client():
     client = TradingClient("key-id", "secret-key")
     return client
 
-
-@pytest.fixture
-def stock_client():
-    client = StockHistoricalDataClient("key-id", "secret-key")
-    return client
-
-
-@pytest.fixture
-def news_client():
-    client = NewsClient("key-id", "secret-key")
-    return client
-
-
-@pytest.fixture
-def corporate_actions_client():
-    client = CorporateActionsClient("key-id", "secret-key")
-    return client
-
-
-@pytest.fixture
-def raw_stock_client():
-    raw_client = StockHistoricalDataClient("key-id", "secret-key", raw_data=True)
-    return raw_client
-
-
-@pytest.fixture
-def crypto_client():
-    client = CryptoHistoricalDataClient("key-id", "secret-key")
-    return client
-
-
-@pytest.fixture
-def option_client() -> OptionHistoricalDataClient:
-    client = OptionHistoricalDataClient("key-id", "secret-key")
-    return client
-
-
-@pytest.fixture
-def screener_client():
-    return ScreenerClient("key-id", "secret-key")
-
-
-@pytest.fixture
-def raw_screener_client():
-    return ScreenerClient("key-id", "secret-key", raw_data=True)
-
-
-@pytest.fixture
-def raw_crypto_client():
-    raw_client = CryptoHistoricalDataClient("key-id", "secret-key", raw_data=True)
-    return raw_client
